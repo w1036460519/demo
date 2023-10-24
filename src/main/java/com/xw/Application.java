9fd10cbda473fd0d27fc7e895fb5529ada97de37
“wanglianjiang <lianjiang.wang@epro.com.cn>
2023-10-24 09:58:22 +0800

    首次提交

---
+++ src/main/java/com/xw/Application.java
@@ -0,0 +1,19 @@
+package com.xw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ *
+ * @Author 王连江
+ * @Date 2023/10/24
+ **/
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
